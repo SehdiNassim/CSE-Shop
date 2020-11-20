00e93af40d179e6a54353aa15dacde6d523966e0
Sehdi Nassim <hm_sehdi@esi.dz>
2020-11-20 01:19:04 +0100

    Deleted what is cse section

--- src/Pages/landingPage.js
+++ src/Pages/landingPage.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import Page from './pageTemplate';
 import Hero from './../Components/Presentational/LandingPage/hero';
-import WhatIsCse from './../Components/Presentational/LandingPage/whatIsCse';
 import LatestProduct from './../Components/Presentational/LandingPage/latestProduct';
 import FeaturedProduct from './../Components/Presentational/LandingPage/featuredProduct';
 
 const LandingPage = () => {
     return <Page>
         <Hero></Hero>
-        <WhatIsCse></WhatIsCse>
         <LatestProduct></LatestProduct>
         <FeaturedProduct></FeaturedProduct>
     </Page>
