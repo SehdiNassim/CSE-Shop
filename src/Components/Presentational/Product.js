5cad52485ebcb109b816b11e7d67985458c5b634
z-sohaib <js_zouambia@esi.dz>
2020-11-24 10:25:16 +0100

    Fixed Hero Section

--- src/Components/Presentational/Product.js
+++ src/Components/Presentational/Product.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react'
 
 export class Product extends Component {
-    constructor(props) {
-        super(props);
-    }
-
 
     render() {
         return (
             <div className="col-12 col-lg-4 row justify-content-center text-center">
                 <div className="col-12">
-                    <img src={this.props.product.src}/>
+                    <img src={this.props.product.src} alt="product"/>
                 </div>
                 <div className="col-12">
                     <p>{this.props.product.title}</p>
