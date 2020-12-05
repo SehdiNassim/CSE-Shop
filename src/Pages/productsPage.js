c770bb5295473845c3bffeee694590e0071106b6
Sehdi Nassim <hm_sehdi@esi.dz>
2020-12-05 01:22:13 +0100

    Added detailed product component

--- src/Pages/productsPage.js
+++ src/Pages/productsPage.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react'
 import Similar from './../Components/Presentational/ProductPage/similar'
+import DetailedProduct from './../Components/Presentational/ProductPage/detailedProduct';
 
 
 const ProductPage=()=>{
     return <Fragment>
+        <DetailedProduct/>
         <Similar/>
     </Fragment>
 }
